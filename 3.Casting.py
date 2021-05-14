a7d9540c7e9045b0c74bb78f44493fb74b2cd1e8
antonycarrasco <antonycarrasco24@gmail.com>
2021-05-14 16:54:54 -0500

    Casting de valores primitivos

---
+++ 3.Casting.py
@@ -0,0 +1,21 @@
+
+
+numero = int(input("Escribe un numero: "))
+
+if(numero ==5):
+    print("El numero es 5")
+else:
+    print("El numero no es 5")
+
+print(type(numero))
+
+
+numero_float = 6.5
+
+print(type(numero_float))
+
+numero_entero = int(numero_float)
+
+print(type(numero_entero))
+
+numero_string = str(numero_float)
