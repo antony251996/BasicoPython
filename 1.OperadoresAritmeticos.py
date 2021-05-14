cd23ff1f91a72b46cda8a9a357617b7e8cb4ff0c
antonycarrasco <antonycarrasco24@gmail.com>
2021-05-14 16:29:08 -0500

    Operadores Aritmeticos

---
+++ 1.OperadoresAritmeticos.py
@@ -0,0 +1,32 @@
+#Suma
+5 + 5
+
+#Si queremos imprimit algun valor
+print(5 + 5)
+
+#Resta
+9 - 5
+print(9-5)
+
+#Multiplicacion
+5 * 5
+print(5*5)
+
+#Division
+16/2
+print(16/2)
+
+#Cociente
+16//3 #Devuelve la parte entera de la division
+
+#Residuo
+21%5 #DEvuelve el residuo de la división
+
+#Potencia
+3 ** 3
+
+#Raiz cuadrada
+9 ** 0.5
+
+import math
+math.sqrt(9)
