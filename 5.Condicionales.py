3c62b9d3a711b6b8f4925b7a0857f490952cbca2
antonycarrasco <antonycarrasco24@gmail.com>
2021-05-15 11:33:36 -0500

    Condicionales

---
+++ 5.Condicionales.py
@@ -0,0 +1,16 @@
+edad = int(input('Ingresa tu edad: '))
+
+#Sentencia if
+if edad >=18:
+    print('Eres mayor de edad!!')
+else:
+    print('Eres menor de edad!!')
+
+numero = int(input('Escribe un numero: '))
+
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
