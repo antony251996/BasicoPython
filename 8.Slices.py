cd99109d04f8d65453378ded44b8b665df751f95
antonycarrasco <antonycarrasco24@gmail.com>
2021-05-15 12:25:41 -0500

    Trabajando con slices

---
+++ 8.Slices.py
@@ -0,0 +1,15 @@
+
+#Slices o rebanadas
+
+nombre = 'Francisco'
+
+#Podemos obviar el primer y ultimo valor si vamos hasta el final
+print(nombre[0:3])
+print(nombre[:3])
+print(nombre[3:])
+
+#Podemos indicar la cantidad de pasos
+print(nombre[1:7:2])
+
+#Podemos ir al revez
+print(nombre[::-1])
