322e11f068df303fc11f1f0f97a4021202fb6057
antonycarrasco <antonycarrasco24@gmail.com>
2021-05-15 12:06:39 -0500

    Funciones

---
+++ 6.Funciones.py
@@ -0,0 +1,14 @@
+def imprimir_mensaje():
+    print('Mensaje especial')
+    print('Estoy aprendiendo funciones')
+    
+
+imprimir_mensaje()
+
+opcion = input('Elige una opcion (1, 2, 3)')
+
+def imprimir_opcion(opcion):
+    print('Hola')
+    print('Como Estás?')
+    print(f'Elegiste la opcion {opcion}')
+    print('Adios')
